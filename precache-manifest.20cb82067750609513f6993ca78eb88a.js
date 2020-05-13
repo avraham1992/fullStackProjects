66fbb57238bba11f4462da9bc9c035a55f8723f8
Avraham Chababo <estyk4@gmail.com>
2020-05-14 02:04:24 +0300

    Updates

---
+++ precache-manifest.20cb82067750609513f6993ca78eb88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a54d59d6bf536ed5715929612ac803",
+    "url": "/fullStackProjects/index.html"
+  },
+  {
+    "revision": "6175d392ff277281b7c0",
+    "url": "/fullStackProjects/static/css/main.9d151e74.chunk.css"
+  },
+  {
+    "revision": "a544615e084b4949818d",
+    "url": "/fullStackProjects/static/js/2.b1f4f90f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fullStackProjects/static/js/2.b1f4f90f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6175d392ff277281b7c0",
+    "url": "/fullStackProjects/static/js/main.f792b43e.chunk.js"
+  },
+  {
+    "revision": "cbb0f91815bc1438bb94",
+    "url": "/fullStackProjects/static/js/runtime-main.4dd85d9d.js"
+  }
+]);
