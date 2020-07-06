0516dfef82b0c74a26760e36bc8afa7df17c9e7e
Avraham Chababo <estyk4@gmail.com>
2020-07-06 22:58:35 +0300

    finished blog - homework6

--- src/BackEnd/Server.py
+++ src/BackEnd/Server.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, abort, make_response
 import mysql.connector as mysql
 from flask_cors import CORS
 import json
+import bcrypt
+import uuid
 
 db = mysql.connect(
 	host = "localhost",
@@ -12,6 +14,18 @@ db = mysql.connect(
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/getPostById/<id>', methods=['GET'])
+def getPostById(id):
+    query = "select id, title, content, author, published, image from posts where id = %s"
+    values = (id,)
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    record = cursor.fetchone()
+    headers = ['id', 'title', 'content', 'author', 'published', 'image']
+    cursor.close()
+    return json.dumps(dict(zip(headers, record)))
+
+
 @app.route('/posts/', methods=['GET', 'POST'])
 def manage_posts():
 	if request.method == 'GET':
@@ -20,6 +34,93 @@ def manage_posts():
 		return add_post()
 	else:
 	    return "{msg: something went wrong with the /posts/ route}"
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    cursor = db.cursor()
+    queryOne = "select id from users where username=%s"
+    valuesOne = (username,)
+    cursor.execute(queryOne, valuesOne)
+    record = cursor.fetchone()
+    if record:
+        cursor.close()
+        abort(401)
+    else:
+        queryTwo = 'insert into users (username, password) values (%s, %s)'
+        hashPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        valuesTwo = (username, hashPassword)
+        cursor.execute(queryTwo, valuesTwo)
+        db.commit()
+        cursor.close()
+        session_id = str(uuid.uuid4())
+        response = make_response()
+        response.set_cookie("session_id", session_id)
+        return response
+
+@app.route('/id', methods=['POST'])
+def id():
+    data = request.get_json()
+    print(data)
+    query = "select id from users where username=%s"
+    values = (data['username'], )
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    record = cursor.fetchone()
+    if not record:
+        cursor.close()
+        abort(401)
+
+    header = ['id']
+    jsonData = []
+    jsonData.append(dict(zip(header, record)))
+    return json.dumps(jsonData)
+
+
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    data = request.get_json()
+    query = "delete from sessions where user_id = %s"
+    values = (data['id'],)
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    db.commit()
+    cursor.close()
+    return "Success!"
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    query = "select id, username, password from users where username = %s"
+    inputUsername = data['username']
+    values = (inputUsername, )
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    record = cursor.fetchone()
+    if not record:
+        cursor.close()
+        abort(401)
+
+    inputPassword = data['password']
+    passDB = record[2].encode('utf-8')
+    if (bcrypt.hashpw(inputPassword.encode('utf-8'), passDB)) != passDB:
+        cursor.close()
+        abort(403)
+
+    session_id = str(uuid.uuid4())
+    user_id = record[0]
+    sessionQuery = "insert into sessions (user_id, session_id) values (%s, %s) on duplicate key update session_id=%s"
+    sessionValues = (user_id, session_id, session_id)
+    cursor.execute(sessionQuery, sessionValues)
+    db.commit()
+    response = make_response()
+    response.set_cookie("session_id", session_id)
+    cursor.close()
+    return response
 
 def add_post():
 	data = request.get_json()
